9d61603213d2fa8a812deebe882c500bb7acb853
ZhangYong19800721 <24452861@qq.com>
2021-04-16 10:01:39 +0800

    update test Gu Frame add Train SPWGAN MMSE

--- SR_CycleWGAN/step06_Test_Gu_Frame.py
+++ SR_CycleWGAN/step06_Test_Gu_Frame.py
@@ -1,5 +1,4 @@
-from sys import argv
-import matplotlib.pyplot as plt
+import argparse
 import pickle
 import torch
 import torch.nn as nn
@@ -7,14 +6,17 @@ import tools
 import torchvision.transforms as transforms
 import torchvision.datasets as dset
 import random
-import torchvision.transforms.functional as ttf
 
 if __name__ == "__main__":
     # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")  # get the GPU device
     ##########################################################################
     ## load the AI model
-    script, _dataroot, _modelGuFile = argv
-    modelGu_file = open(_modelGuFile, "rb")  # open the model file
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataroot", type=str, help="The root dir for dataset")
+    parser.add_argument("--ModelGuFile", type=str, help="None or the path for Gu model")
+    args = parser.parse_args()
+
+    modelGu_file = open(args.ModelGuFile, "rb")  # open the model file
     modelGu = pickle.load(modelGu_file)  # load the model file
     if isinstance(modelGu, nn.DataParallel):
         modelGu = modelGu.module
@@ -23,7 +25,7 @@ if __name__ == "__main__":
     modelGu_file.close()  # close the model file
 
     image_H, image_W = 1080, 1920
-    dataroot = _dataroot
+    dataroot = args.dataroot
     dataset = dset.ImageFolder(root=dataroot, transform=transforms.Compose([transforms.Resize((image_H, image_W))]))
     n = random.randint(0, len(dataset)-1)
     original_image = dataset[n][0]
